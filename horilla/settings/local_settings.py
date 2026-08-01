a886e120be42e1f11ac5590df42bb35560787595
Rredza8 <rezaashary8@gmail.com>
2026-08-01 22:19:59 +0330

    fa msg translated

---
+++ horilla/settings/local_settings.py
@@ -0,0 +1,9 @@
+
+
+ALLOWED_LANGUAGES = [
+    ("en", "English", "usa.webp"),
+    ("ar", "Arabic", "ar.png"),
+    ("de", "German", "german.jpeg"),
+    ("fr", "French", "france.webp"),
+    ("fa", "Persian", "iran.svg"),
+]
