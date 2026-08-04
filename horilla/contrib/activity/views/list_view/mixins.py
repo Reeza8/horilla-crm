86608a3333fe7b56a13c380628ecd8b0fc190255
Horilla <support@horilla.com>
2026-08-04 11:10:44 +0530

    [UPDT] ACTIVITY: Prioritize duplicate action

--- horilla/contrib/activity/views/list_view/mixins.py
+++ horilla/contrib/activity/views/list_view/mixins.py
@@ -53,6 +53,18 @@ COMMON_ACTIONS = [
                     """,
     },
     {
+        "action": _("Duplicate"),
+        "src": "assets/icons/duplicate.svg",
+        "img_class": "w-4 h-4",
+        "permission": "activity.add_activity",
+        "attrs": """
+                        hx-get="{get_activity_edit_url}?duplicate=true"
+                        hx-target="#modalBox"
+                        hx-swap="innerHTML"
+                        onclick="openModal()"
+                        """,
+    },
+    {
         "action": "Delete",
         "src": "assets/icons/a4.svg",
         "img_class": "w-4 h-4",
@@ -65,18 +77,6 @@ COMMON_ACTIONS = [
                     hx-vals='{{"check_dependencies": "true"}}'
                     onclick="openDeleteModeModal()"
                 """,
-    },
-    {
-        "action": _("Duplicate"),
-        "src": "assets/icons/duplicate.svg",
-        "img_class": "w-4 h-4",
-        "permission": "activity.add_activity",
-        "attrs": """
-                        hx-get="{get_activity_edit_url}?duplicate=true"
-                        hx-target="#modalBox"
-                        hx-swap="innerHTML"
-                        onclick="openModal()"
-                        """,
     },
 ]
 
