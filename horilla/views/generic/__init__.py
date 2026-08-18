cb0c76dcc0bace89464d779e350a7a5c5da8ce39
Horilla <support@horilla.com>
2026-08-18 12:27:45 +0530

    [UPDT] CORE: Re-export Horilla generic DetailView from views.generic DetailView was added in horilla/views/generic/detail.py but not exported from the package, so callers still had to import the module path instead of `from horilla.views.generic import DetailView`.

--- horilla/views/generic/__init__.py
+++ horilla/views/generic/__init__.py
@@ -3,12 +3,6 @@
 from horilla.views.generic.edit import FormView
 from horilla.views.generic.base import View, TemplateView
 from horilla.views.generic.list import ListView
+from horilla.views.generic.detail import DetailView
 
-__all__ = (
-    [
-        "FormView",
-        "View",
-        "TemplateView",
-        "ListView",
-    ],
-)
+__all__ = (["FormView", "View", "TemplateView", "ListView", "DetailView"],)
