54d4a25510ca312c70153f058b46565ebebcfe43
Horilla <support@horilla.com>
2026-08-25 12:49:38 +0530

    [UPDT] HORILLA: Remove duplicate RTL injection, clear local settings, fix locale EOF

--- horilla/settings/local_settings.py
+++
@@ -1,7 +0,0 @@
-ALLOWED_LANGUAGES = [
-    ("en", "English", "usa.webp"),
-    ("ar", "Arabic", "ar.png"),
-    ("de", "German", "german.jpeg"),
-    ("fr", "French", "france.webp"),
-    ("fa", "Persian", "iran.svg"),
-]
